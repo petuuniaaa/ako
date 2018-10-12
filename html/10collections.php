c49130c54880a26025f4fcea78313f826b034167
petuuniaaa <celine.debruijne@gmail.com>
2018-10-12 10:34:06 +0200

    Modif initialisation

---
+++ html/10collections.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tes collections</title>
+</head>
+<body>
+    <header>
+        <h1>Tes collections</h1>
+    </header>
+    <section>
+        <button>Monuments</button>
+        <button>Animaux</button>
+        <button>Mythes et Héros</button>
+        <button>Drapeaux</button>
+    </section>
+    <img src="" alt="pieuvre">
+    <img src="" alt="bulle">
+</body>
+</html>
---
+++ html/3pseudo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pseudo</title>
+</head>
+<body>
+    <img src="" alt="pieuvre">
+    <img src="" alt="bulle">
+    <form action="pseudo_reception.php" method="get">
+        <input type="text" name="pseudo" placeholder="Ton pseudo" maxlength="20" required>
+        <input type="submit" value="OK">
+    </form>
+</body>
+</html>
