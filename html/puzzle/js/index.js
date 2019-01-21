46dbfd6232d1cd13e3dd97c9f56351bf647da625
petuuniaaa <celine.debruijne@gmail.com>
2019-01-21 14:00:40 +0100

    #MiJanvier2019

--- html/puzzle/js/index.js
+++ html/puzzle/js/index.js
@@ -2,18 +2,18 @@
 
 var piezas = document.getElementsByClassName('movil');
 
-var tamWidh = [134,192,134,163,134,163,134,192,134];
-var tamHeight = [163,134,163,134,192,134,163,134,163];
+var tamWidh = [134, 192, 134, 163, 134, 163, 134, 192, 134];
+var tamHeight = [163, 134, 163, 134, 192, 134, 163, 134, 163];
 
-for(var i=0;i<piezas.length;i++){
+for (var i = 0; i < piezas.length; i++) {
 	piezas[i].setAttribute("width", tamWidh[i]);
-	piezas[i].setAttribute("height",tamHeight[i]);
+	piezas[i].setAttribute("height", tamHeight[i]);
 	piezas[i].setAttribute("x", Math.floor((Math.random() * 10) + 1));
 	piezas[i].setAttribute("y", Math.floor((Math.random() * 409) + 1));
-	piezas[i].setAttribute("onmousedown","seleccionarElemento(evt)");
+	piezas[i].setAttribute("onmousedown", "seleccionarElemento(evt)");
 }
 
-var elementSelect = 0;  
+var elementSelect = 0;
 var currentX = 0;
 var currentY = 0;
 var currentPosX = 0;
@@ -21,30 +21,30 @@ var currentPosY = 0;
 
 function seleccionarElemento(evt) {
 	elementSelect = reordenar(evt);
-	currentX = evt.clientX;        
+	currentX = evt.clientX;
 	currentY = evt.clientY;
-	currentPosx = parseFloat(elementSelect.getAttribute("x"));     
+	currentPosx = parseFloat(elementSelect.getAttribute("x"));
 	currentPosy = parseFloat(elementSelect.getAttribute("y"));
-	elementSelect.setAttribute("onmousemove","moverElemento(evt)");
+	elementSelect.setAttribute("onmousemove", "moverElemento(evt)");
 }
 
-function moverElemento(evt){
+function moverElemento(evt) {
 	var dx = evt.clientX - currentX;
 	var dy = evt.clientY - currentY;
 	currentPosx = currentPosx + dx;
 	currentPosy = currentPosy + dy;
-	elementSelect.setAttribute("x",currentPosx);
-	elementSelect.setAttribute("y",currentPosy);
-	currentX = evt.clientX;        
+	elementSelect.setAttribute("x", currentPosx);
+	elementSelect.setAttribute("y", currentPosy);
+	currentX = evt.clientX;
 	currentY = evt.clientY;
-	elementSelect.setAttribute("onmouseout","deseleccionarElemento(evt)");
-	elementSelect.setAttribute("onmouseup","deseleccionarElemento(evt)");
+	elementSelect.setAttribute("onmouseout", "deseleccionarElemento(evt)");
+	elementSelect.setAttribute("onmouseup", "deseleccionarElemento(evt)");
 	iman();
 }
 
-function deseleccionarElemento(evt){
+function deseleccionarElemento(evt) {
 	testing();
-	if(elementSelect != 0){			
+	if (elementSelect != 0) {
 		elementSelect.removeAttribute("onmousemove");
 		elementSelect.removeAttribute("onmouseout");
 		elementSelect.removeAttribute("onmouseup");
@@ -54,7 +54,7 @@ function deseleccionarElemento(evt){
 
 var entorno = document.getElementById('entorno');
 
-function reordenar(evt){
+function reordenar(evt) {
 	var padre = evt.target.parentNode;
 	var clone = padre.cloneNode(true);
 	var id = padre.getAttribute("id");
@@ -63,32 +63,32 @@ function reordenar(evt){
 	return entorno.lastChild.firstChild;
 }
 
-var origX = [200,304,466,200,333,437,200,304,466];   
-var origY = [100,100,100,233,204,233,337,366,337];
+var origX = [200, 304, 466, 200, 333, 437, 200, 304, 466];
+var origY = [100, 100, 100, 233, 204, 233, 337, 366, 337];
 
-function iman(){
-	for(var i=0;i<piezas.length;i++){
-		if (Math.abs(currentPosx-origX[i])<15 && Math.abs(currentPosy-origY[i])<15) {
-			elementSelect.setAttribute("x",origX[i]);
-			elementSelect.setAttribute("y",origY[i]);
+function iman() {
+	for (var i = 0; i < piezas.length; i++) {
+		if (Math.abs(currentPosx - origX[i]) < 15 && Math.abs(currentPosy - origY[i]) < 15) {
+			elementSelect.setAttribute("x", origX[i]);
+			elementSelect.setAttribute("y", origY[i]);
 		}
 	}
 }
-			
+
 var win = document.getElementById("win");
 
 function testing() {
 	var bien_ubicada = 0;
 	var padres = document.getElementsByClassName('padre');
-	for(var i=0;i<piezas.length;i++){
-		var posx = parseFloat(padres[i].firstChild.getAttribute("x"));    
+	for (var i = 0; i < piezas.length; i++) {
+		var posx = parseFloat(padres[i].firstChild.getAttribute("x"));
 		var posy = parseFloat(padres[i].firstChild.getAttribute("y"));
 		ide = padres[i].getAttribute("id");
-		if(origX[ide] == posx && origY[ide] == posy){
+		if (origX[ide] == posx && origY[ide] == posy) {
 			bien_ubicada = bien_ubicada + 1;
 		}
 	}
-	if(bien_ubicada == 9){
+	if (bien_ubicada == 9) {
 		win.play();
 	}
 }
