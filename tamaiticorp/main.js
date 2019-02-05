35bcb6f22d0de7792cc5b6cdd84576a6fb364f46
Aurélien Duberville <aurelien.duberville@gmail.com>
2019-02-05 22:13:35 +0100

    js dynamique

--- tamaiticorp/main.js
+++ tamaiticorp/main.js
@@ -8,14 +8,45 @@ setTimeout(() => {
     $('header').css({'opacity':'1','transition':'1s ease'})
 }, 800);
 
-var equipe = $('#equipe');
+var equipeimg = $('#equipe img');
+var equipetxt = $('#equipe p');
+var video = $('.presentation');
+var projet = $('#projet');
+var jouer = $('#jouer');
+var coloriages = $('#coloriages');
+$(equipeimg).css('opacity','0');
+$(equipetxt).css('opacity','0');
+$(video).css('opacity','0');
+$(projet).css('opacity','0');
+$(jouer).css('opacity','0');
+$(coloriages).css('opacity','0');
 
-$(equipe).css('opacity','0');
 
+$(document).scroll(function(){
+var maxheight = $(document).height();
+var current = $(document).scrollTop() - $(window).height();
+var percent = (100 * (current + $(window).height())) / (maxheight - $(window).height());
+console.log('percent = ' + percent);
 
-
-$(window).scroll(function(){
-
-    $(equipe).css({'opacity':'1','transition':'2.5s ease'})
+if (percent >= 10 && percent < 15 ){
+    $(equipeimg).css({'opacity':'1','transition':'1.5s ease'})
+    $(equipetxt).css({'opacity':'1','transition':'2.5s ease'})
+}
+if (percent >= 25 && percent < 30 ){
+    $(video).css({'opacity':'1','transition':'1.5s ease'})
     
+}
+if (percent >=45 && percent < 55 ){
+    $(projet).css({'opacity':'1','transition':'1.5s ease'})
+    
+}
+if (percent >=65 && percent < 75 ){
+    $(jouer).css({'opacity':'1','transition':'1.5s ease'})
+    
+}
+if (percent >=90 && percent < 100 ){
+    $(coloriages).css({'opacity':'1','transition':'1.5s ease'})
+    
+}
+
 })
