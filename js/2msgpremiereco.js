077bfecb12ab59dfcffb0c68f33dd9c4d925a9b2
Aurélien Duberville <aurelien.duberville@gmail.com>
2019-03-11 10:44:27 +0100

    jquery transition

---
+++ js/2msgpremiereco.js
@@ -0,0 +1,12 @@
+
+
+setTimeout(() => {
+    $('.conversation').css({
+        'opacity': '1',
+        'transition': '1s ease'
+    })
+    $('button').css({
+        'opacity': '1',
+        'transition': '1s ease'
+    })
+}, 500);
