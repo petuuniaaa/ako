6b945c41debbe2745d5c4138fab514fe3d0cebc1
petuuniaaa <debruijneceline@gmail.com>
2019-03-17 17:14:27 +0100

    Puzzle piece random position + css

--- html/puzzle/puzzle.js
+++ html/puzzle/puzzle.js
@@ -1,93 +1,59 @@
-// $(function(){
-// $('img').draggable();
-// $( 'th' ).droppable({
-//     drop: function( event, ui ) {
-//       $( this )
-//         .addClass('fond')
-//         .find( "th" )
-//           .html( "Dropped!" );
-//     }
-// })
-// });
-// var images = $(function(){
-//     $('img').draggable();
-//     var img = $(this);
-//     var imgId = $('img').attr('id');
-   
+function RandomPlace() {
+  var mintop = 0;
+  var maxtop = 50;
+  var minleft = 0;
+  var maxleft = 80;
+  tabdiv = document.getElementsByClassName('imgRandom');
+  var i = 0;
+  while (tabdiv[i++]) {
+    tabdiv[i - 1].style.top = mintop + Math.floor(Math.random() * (maxtop - mintop)) + 'px';
+    tabdiv[i - 1].style.left = minleft + Math.floor(Math.random() * (maxleft - minleft)) + 'px';
+    tabdiv[i - 1].style.zIndex = i;
+  }
+}
 
-//     });;
+$(function () {
 
-// var ths = $('th');
-// for(i=0;i < ths[i].lenght; i++){
-//     $(ths[i]).droppable({
-
-//         accept : 'img#' + $(ths[i]).attr('id')
-        // drop: function( event, ui ) {
-        //     var img = $(this);
-        //     var idcase = $(this).attr('id');
-         
-        //     if(img.attr('data-case') == $('th').attr('id')){
-        //         $('th').append(img);
-        //         $( this )
-        //         .addClass('fond')
-        //         .find( "th" )
-            
-        //     }
-        // var thId =  $('th').attr('id');
-        
-             
-    
-//     });
-// }
-
-// if( images == cases ){
-//     $(imgId).css('display','none');
-//     $(thId).css('background-color','red');
-// }
-
-$(function(){
-    // $('.img').draggable({});
-    
   var ths = $('th');
-  
-  var imgs = $('img');
+  var imgs = $('.imgRandom');
+  var completed = false;
 
-  var imgid = $('img').attr('id');
-  
-  ths.map(function(idx, item){
-      
+  ths.map(function (idx, item) {
+
     var th = $(item);
-    
+
     th.droppable({
-      
+
       accept: '[data-case="' + th.attr('id') + '"]',
-      
-      drop: function(){
-          var img =$('[data-case="' + th.attr('id') + '"]').attr('src');
-          console.log('[data-case="' + th.attr('id') + '"]');
-        th.css({'background-image':'url('+img+')','background-size':'center','background-repeat':'no-repeat'});
-        $('[data-case="' + th.attr('id') + '"]').css('display','none');
 
-        // th.css('background-color','yellowgreen');
-      // th.css({'background-image':$((img)),'background-size':'center'});
+      drop: function () {
+        var img = $('[data-case="' + th.attr('id') + '"]').attr('src');
+        th.css({
+          'background-image': 'url(' + img + ')',
+          'background-size': 'center',
+          'background-repeat': 'no-repeat'
+        });
+        $('[data-case="' + th.attr('id') + '"]').css('display', 'none');
+
         img.draggable('disable');
-        // $('[data-case="' + th.attr('id') + '"]').css('display','none');
-     
+
       }
-      
+
     });
-    
+
   })
-  
-  imgs.map(function(idx, item){
-      
+
+  imgs.map(function (idx, item) {
+
     var img = $(item);
-    
+
     img.draggable();
-    
   })
 
-  
-  
-})
+  // if()
 
+  // if (completed === true) {
+  //   $('table').css('border-spacing', '0');
+  // }
+
+})
